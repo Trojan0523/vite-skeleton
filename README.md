2b0999e5b5a1701f503525a2cecd3dc2f427d1d5
Trojan0523 <1153532327@qq.com>
2022-07-22 20:45:09 +0800

    chore: bff side finished

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 - Trojan初始化Client端 模板
 
+1. express-ts & vite
+
+2. nest & vite
+
 - esno 解析ts
+
 - swc es6 -> es5 升降级
--- apps/vite-express/server/utils/index.ts
+++ apps/vite-express/server/utils/index.ts
@@ -1,8 +1,14 @@
-/* eslint no-console: 0 */
-
+import readline from 'readline'
 import color from 'picocolors'
 
 export function printServerUrls (urls) {
-
+    
 }
 
+export function clearScreen () {
+    const repeatCount = process.stdout.rows - 2
+    const blank = repeatCount > 0 ? '\n'.repeat(repeatCount) : ''
+    console.log(blank)
+    readline.cursorTo(process.stdout, 0, 0)
+    readline.clearScreenDown(process.stdout)
+}
